10a7c7c86f780458980717eaaa92e5626589a949
Qing Ma <gsmofgsm@hotmail.com>
2021-01-03 14:36:54 +0100

    joselfonseca/lighthouse-graphql-passport-auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,8 +23,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Passport::routes(function ($router) {
-            $router->forAccessTokens();
-        });
     }
 }
